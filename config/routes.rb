58d54356237a0f3238780c8bf78c079a316fcabb
B V Satyaram <bvsatyaram@gmail.com>
2016-06-23 07:02:49 +0530

    Allow cross origin requests

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  match '*any' => 'application#options', :via => [:options]
   resources :tasks, defaults: {format: :json}, only: [:index, :create, :destroy]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
